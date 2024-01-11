d5beda5e6e5d6c85bf60335877b24157a58c7164
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-01-11 22:04:06 +0200

    Add first home view, home controller and routes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 
+const routes = require('./routes');
+
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'src/views');
 
 const PORT = 5000;
+
+app.use(routes);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
