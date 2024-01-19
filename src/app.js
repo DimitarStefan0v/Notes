022174d2e7dab9dd021799933e613adeeb390b6c
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-01-19 17:40:46 +0200

    Add mongoose and extract the db config in separate file

--- src/app.js
+++ src/app.js
@@ -1,19 +1,24 @@
 const path = require('path');
 
 const express = require('express');
+const dbConnect = require('./config/dbConfig');
 
 const routes = require('./routes');
 
 const app = express();
+const PORT = 5000;
 
 // ejs config
 app.set('view engine', 'ejs');
 app.set('views', 'src/views');
 
 // express config
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: false }));
 app.use(routes);
 
-const port = 5000;
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+dbConnect()
+	.then(() => console.log('DB connected successfully'))
+	.catch(err => console.log('DB error ', err));
+
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
