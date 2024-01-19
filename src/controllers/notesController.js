b0905e64bc3d02d7fc44eff5b9a5f1aaa7ddc4d4
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-01-19 19:17:10 +0200

    Add the service func on the post request

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,24 +1,28 @@
 const express = require('express');
 
 const router = express.Router();
-const notes = require('../utils/data');
+
+const notesService = require('../services/notesService');
 
 router.get('/create', async (req, res) => {
 	const pageTitle = 'Create';
-    const path = '/notes/create';
+	const path = '/notes/create';
+
 	res.render('create', { pageTitle, path });
 });
 
 router.post('/create', async (req, res) => {
-	const note = req.body.note;
-	notes.push(note);
-	res.redirect('/notes/all');
+	const title = req.body.title;
+	const desc = req.body.description;
+	const note = { title: title, description: desc };
+    notesService.create(note);
+	res.redirect('/');
 });
 
 router.get('/all', async (req, res) => {
-    const pageTitle = 'Notes';
-    const path = '/notes/all'
-    res.render('notes', { pageTitle, path, notes });
+	const pageTitle = 'Notes';
+	const path = '/notes/all';
+	res.render('notes', { pageTitle, path });
 });
 
 module.exports = router;
