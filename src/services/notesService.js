eaa87a2909b180d0779eb349bcd2c557ee3d8ab8
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-01-24 17:54:38 +0200

    Add delete service method for notes

--- src/services/notesService.js
+++ src/services/notesService.js
@@ -13,3 +13,5 @@ exports.getAll = () => Note.find().lean();
 exports.getById = (noteId) => Note.findById(noteId).lean();
 
 exports.update = (noteId, noteData) => Note.findByIdAndUpdate(noteId, noteData);
+
+exports.delete = (noteId) => Note.findByIdAndDelete(noteId);
