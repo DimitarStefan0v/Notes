f8b9e3a08c06f0b4624260bee7677d7dc2a5f8ff
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-01-25 18:45:33 +0200

    Refactoring the controllers a bit

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -3,9 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    const pageTitle = 'Home';
-    const path = '/';
-	res.render('home/home', { pageTitle, path });
+	res.render('home/home', { pageTitle: 'Home', path: '/' });
 });
 
 module.exports = router;
--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -5,54 +5,41 @@ const router = express.Router();
 const notesService = require('../services/notesService');
 
 router.get('/create', (req, res) => {
-	const pageTitle = 'Create';
-	const path = '/notes/create';
-
-	res.render('notes/create', { pageTitle, path });
+	res.render('notes/create', { pageTitle: 'Create Note', path: '/create' });
 });
 
 router.post('/create', async (req, res) => {
 	const { title, description } = req.body;
-	const pageTitle = 'Create';
-	const path = '/notes/create';
 
 	try {
 		await notesService.create({ title, description });
 	} catch (error) {
 		console.log(error.message);
-		return res.render('notes/create', { pageTitle, path });
+		return res.render('notes/create', { pageTitle: 'Create Note', path: '/create' });
 	}
 
 	res.redirect('/notes/all');
 });
 
 router.get('/all', async (req, res) => {
-	const pageTitle = 'Notes';
-	const path = '/notes/all';
 	const notes = await notesService.getAll();
 
-	res.render('notes/notes', { pageTitle, path, notes });
+	res.render('notes/notes', { pageTitle: 'Notes', path: '/all', notes });
 });
 
 router.get('/:noteId/details', async (req, res) => {
-	const pageTitle = 'Note Details';
-	const path = '/notes/all';
 	const note = await notesService.getById(req.params.noteId);
 
-	res.render('notes/details', { pageTitle, path, note });
+	res.render('notes/details', { pageTitle: 'Note Details', path: '', note });
 });
 
 router.get('/:noteId/update', async (req, res) => {
-	const pageTitle = 'Note Update';
-	const path = '/notes/all';
 	const note = await notesService.getById(req.params.noteId);
 
-	res.render('notes/update', { pageTitle, path, note });
+	res.render('notes/update', { pageTitle: 'Update Note', path: '', note });
 });
 
 router.post('/:noteId/update', async (req, res) => {
-	const pageTitle = 'Note Update';
-	const path = '/notes/all';
 	const noteId = req.params.noteId;
 	const { title, description } = req.body;
 	const note = { title, description };
@@ -62,21 +49,21 @@ router.post('/:noteId/update', async (req, res) => {
 
 		// TODO: db validation is not triggered on update
 	} catch (error) {
-		return res.render('notes/update', { pageTitle, path, note });
+		return res.render('notes/update', { pageTitle: 'Update Note', path: '', note });
 	}
 
 	res.redirect(`/notes/${noteId}/details`);
 });
 
 router.get('/:noteId/delete', async (req, res) => {
-    try {
-        await notesService.delete(req.params.noteId);
-    } catch (error) {
-        console.log(error.message);
-        return res.redirect('/');
-    }
+	try {
+		await notesService.delete(req.params.noteId);
+	} catch (error) {
+		console.log(error.message);
+		return res.redirect('/');
+	}
 
-    res.redirect('/notes/all');
+	res.redirect('/notes/all');
 });
 
 module.exports = router;
