cd01cf2770879f20e8ead4d2d002de5739b5a9b7
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-01-29 13:38:13 +0200

    Add endpoint for register on post action

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -6,4 +6,10 @@ router.get('/register', (req, res) => {
 	res.render('users/register', { pageTitle: 'Register', path: '/register' });
 });
 
+router.post('/register', (req, res) => {
+	const { username, email, password, repeatPassword } = req.body;
+	console.log(username, email, password, repeatPassword);
+	res.redirect('/');
+});
+
 module.exports = router;
