c44f37aabe093a18fb7dbcec540585358ff60772
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-01-29 18:01:20 +0200

    Add custom validation for email

--- src/models/User.js
+++ src/models/User.js
@@ -4,16 +4,24 @@ const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: [true, 'Username is required'],
-        minLength: [4, 'Username must be at least 4 ']
+        minLength: [4, 'Username must be at least 4 '],
+        
     },
     email: {
         type: String,
-        required: [true, 'Email is required']
+        required: [true, 'Email is required'],
+        validate: {
+            validator: function(value) {
+                return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(value);
+            },
+            message: 'Invalid email address'
+        }
     },
     password: {
         type: String,
         required: [true, 'Password is required'],
-        minLength: [6, 'Password must be at least 6 characters long']
+        minLength: [6, 'Password must be at least 6 characters long'],
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
     }
 });
 
