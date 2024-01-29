0cfacec644e185166ee672e0acac6f6be19050b7
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-01-29 13:33:30 +0200

    Add usersController and endpoint for register on get method

--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,10 @@ const router = express.Router();
 
 const homeController = require('./controllers/homeController');
 const notesController = require('./controllers/notesController');
+const usersController = require('./controllers/usersController');
 
 router.use(homeController);
 router.use('/notes', notesController);
+router.use('/users', usersController);
 
 module.exports = router;
