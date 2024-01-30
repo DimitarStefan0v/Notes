3e80603045d9a2a3c2fb21f1003d3a1e02163184
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-01-30 09:29:55 +0200

    Add cookie-parser and jsonwebtoken

--- src/config/expressConfig.js
+++ src/config/expressConfig.js
@@ -1,12 +1,14 @@
 const path = require('path');
 
 const express = require('express');
+const cookieParser = require('cookie-parser');
 
 const routes = require('../routes');
 
 function expressConfig(app) {
 	app.use(express.static(path.resolve(__dirname, '..', 'public')));
 	app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser());
 	app.use(routes);
 }
 
