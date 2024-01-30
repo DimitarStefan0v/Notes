56ef47211ec1d2e71c1888a88c09f43d360304c7
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-01-30 11:07:57 +0200

    Create jwt token on successful login and return it

--- src/services/usersService.js
+++ src/services/usersService.js
@@ -1,4 +1,7 @@
 const bcrypt = require('bcrypt');
+const dotenv = require('dotenv').config();
+
+const jwtPromises = require('../lib/jwt');
 
 const User = require('../models/User');
 
@@ -7,15 +10,27 @@ exports.register = (userData) => User.create(userData);
 exports.login = async (username, password) => {
 	const user = await User.findOne({ username });
 
-    if (!user) {
-        throw new Error('Sorry, your username or password was incorrect. Please double-check your username and password.');
-    }
+	if (!user) {
+		throw new Error(
+			'Sorry, your username or password was incorrect. Please double-check your username and password.'
+		);
+	}
 
-    const validPassword = await bcrypt.compare(password, user.password);
+	const validPassword = await bcrypt.compare(password, user.password);
 
-    if (!validPassword) {
-        throw new Error('Sorry, your username or password was incorrect. Please double-check your username and password.')
-    }
+	if (!validPassword) {
+		throw new Error(
+			'Sorry, your username or password was incorrect. Please double-check your username and password.'
+		);
+	}
 
-    return user;
+	const payload = {
+		_id: user._id,
+		username: user.username,
+		email: user.email,
+	};
+
+	const token = await jwtPromises.sign(payload, process.env.JWT_SECRET, { expiresIn: '2d' });
+
+	return token;
 };
