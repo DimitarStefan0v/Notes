ae48b0163df531dfa4fa320a73a1a0c4b686732c
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-02-01 18:58:02 +0200

    Add helmet

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const expressConfig = require('./config/expressConfig');
 const ejsConfig = require('./config/ejsConfig');
 const dbConnect = require('./config/dbConfig');
+const routes = require('./routes');
 
 const app = express();
 const PORT = 5000;
@@ -15,5 +16,7 @@ expressConfig(app);
 dbConnect()
 	.then(() => console.log('DB connected successfully'))
 	.catch((err) => console.log('DB error ', err));
+
+app.use(routes);
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
--- src/config/expressConfig.js
+++ src/config/expressConfig.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv').config();
+const helmet = require('helmet');
 
 const { auth } = require('../middlewares/authMiddleware');
 const routes = require('../routes');
@@ -11,8 +12,8 @@ function expressConfig(app) {
 	app.use(express.static(path.resolve(__dirname, '..', 'public')));
 	app.use(express.urlencoded({ extended: false }));
 	app.use(cookieParser());
+    app.use(helmet());
 	app.use(auth);
-	app.use(routes);
 }
 
 module.exports = expressConfig;
