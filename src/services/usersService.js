959741dddf0bf61518cd05826a667e249bd1b801
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-02-04 17:33:44 +0200

    Extract user data validation in separate module

--- src/services/usersService.js
+++ src/services/usersService.js
@@ -2,51 +2,9 @@ const bcrypt = require('bcrypt');
 
 const User = require('../models/User');
 const jwtPromises = require('../lib/jwt');
-const { isRequired } = require('../utils/commonValidation');
 const { ERROR_MESSAGES } = require('../utils/errorMessages');
 
-exports.register = async ({ username, email, password, repeatPassword }) => {
-    const errors = [];
-
-    if (isRequired(username) === false) {
-        errors.push(ERROR_MESSAGES.REQUIRED('Username'));
-    }
-
-    if (isRequired(email) === false) {
-        errors.push(ERROR_MESSAGES.REQUIRED('Email'));
-    }
-
-    if (isRequired(password) === false) {
-        errors.push(ERROR_MESSAGES.REQUIRED('Password'));
-    }
-
-    if (errors.length > 0) {
-        throw errors;
-    }
-
-    username = username.trim();
-    email = email.trim();
-    password = password.trim();
-    repeatPassword = repeatPassword.trim();
-
-	let user = await User.findOne({ username: username });
-
-	if (user) {
-		errors.push(ERROR_MESSAGES.USERNAME_TAKEN);
-	}
-
-	user = await User.findOne({ email: email });
-
-	if (user) {
-		errors.push(ERROR_MESSAGES.EMAIL_TAKEN);
-	}
-
-    if (errors.length > 0) {
-        throw errors;
-    }
-
-	return User.create({ username, email, password, repeatPassword });
-};
+exports.register = ({ username, email, password, repeatPassword }) => User.create({ username, email, password, repeatPassword });
 
 exports.login = async (username, password) => {
 	const user = await User.findOne({ username });
@@ -73,3 +31,5 @@ exports.login = async (username, password) => {
 
 	return token;
 };
+
+
--- src/utils/commonValidation.js
+++ src/utils/commonValidation.js
@@ -1,7 +1,65 @@
-exports.isRequired = (value) => {
-    if (!value || value.length < 1 || /^[\s]+$/.test(value)) {
-        return false;
-    }
+const User = require('../models/User');
+const { ERROR_MESSAGES } = require('./errorMessages');
 
-    return true;
+function isRequired(value) {
+	if (!value || value.length < 1 || /^[\s]+$/.test(value)) {
+		return false;
+	}
+
+	return true;
 };
+
+async function userDataValidation(userData) {
+	const result = {
+        errors: [],
+        newUserData: undefined,
+    };
+
+    // Checking that there is data and most importantly it is not only whitespaces
+
+	if (isRequired(userData.username) === false) {
+		result.errors.push(ERROR_MESSAGES.REQUIRED('Username'));
+	}
+
+	if (isRequired(userData.email) === false) {
+		result.errors.push(ERROR_MESSAGES.REQUIRED('Email'));
+	}
+
+	if (isRequired(userData.password) === false) {
+		result.errors.push(ERROR_MESSAGES.REQUIRED('Password'));
+	}
+
+	if (result.errors.length > 0) {
+		return result;
+	}
+
+	result.newUserData = {
+		username: userData.username.trim(),
+		email: userData.email.trim(),
+		password: userData.password.trim(),
+		repeatPassword: userData.repeatPassword.trim(),
+	};
+
+    // Checking that there is no user with the same username or email in db
+
+	let user = await User.findOne({ username: result.newUserData.username });
+
+	if (user) {
+		result.errors.push(ERROR_MESSAGES.USERNAME_TAKEN);
+	}
+
+	user = await User.findOne({ email: result.newUserData.email });
+
+	if (user) {
+		result.errors.push(ERROR_MESSAGES.EMAIL_TAKEN);
+	}
+
+	return result;
+};
+
+const commonValidation = {
+    isRequired,
+    userDataValidation,
+}
+
+module.exports = commonValidation;
