afbde5776600e7caa5ec213dfcbb0cbc66977e5e
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-02-04 13:25:04 +0200

    Pass error messages and user data when rendering register view

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,21 +1,29 @@
 const express = require('express');
 
-const usersService = require('../services/usersService');
-
 const router = express.Router();
 
+const usersService = require('../services/usersService');
+const { extractErrorMessages } = require('../utils/errorHelpers');
+
 router.get('/register', (req, res) => {
-	res.render('users/register', { pageTitle: 'Register', path: '/register' });
+	res.render('users/register', { pageTitle: 'Register', path: '/register', errorMessages: [], userData: {} });
 });
 
 router.post('/register', async (req, res) => {
+	// TODO if users is already logged in redirect him to 404
 	const { username, email, password, repeatPassword } = req.body;
 	try {
 		await usersService.register({ username, email, password, repeatPassword });
 	} catch (error) {
 		//TODO: pass the error message in the view
-		console.log(error.message);
-		return res.redirect('/users/register');
+		const messages = extractErrorMessages(error);
+		console.log(messages);
+		return res.render('users/register', {
+			pageTitle: 'Register',
+			path: '/register',
+			errorMessages: messages,
+			userData: { username, email, password, repeatPassword },
+		});
 	}
 
 	res.redirect('/');
@@ -41,7 +49,7 @@ router.post('/login', async (req, res) => {
 });
 
 router.get('/logout', (req, res) => {
-    res.clearCookie('auth');
+	res.clearCookie('auth');
 	res.redirect('/');
 });
 
