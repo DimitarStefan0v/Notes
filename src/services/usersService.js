e3727c399502e6ddbd185e8e6ac681409da8026a
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-02-04 13:29:57 +0200

    Rework the user service to throw string array with messages when data is only whitespace and if it's not trim it

--- src/services/usersService.js
+++ src/services/usersService.js
@@ -1,26 +1,64 @@
 const bcrypt = require('bcrypt');
 
-const jwtPromises = require('../lib/jwt');
-
 const User = require('../models/User');
+const jwtPromises = require('../lib/jwt');
+const { isRequired } = require('../utils/commonValidation');
+const { ERROR_MESSAGES } = require('../utils/errorMessages');
 
-exports.register = (userData) => User.create(userData);
+exports.register = async ({ username, email, password, repeatPassword }) => {
+    const errors = [];
+
+    if (isRequired(username) === false) {
+        errors.push(ERROR_MESSAGES.REQUIRED('Username'));
+    }
+
+    if (isRequired(email) === false) {
+        errors.push(ERROR_MESSAGES.REQUIRED('Email'));
+    }
+
+    if (isRequired(password) === false) {
+        errors.push(ERROR_MESSAGES.REQUIRED('Password'));
+    }
+
+    if (errors.length > 0) {
+        throw errors;
+    }
+
+    username = username.trim();
+    email = email.trim();
+    password = password.trim();
+    repeatPassword = repeatPassword.trim();
+
+	let user = await User.findOne({ username: username });
+
+	if (user) {
+		errors.push(ERROR_MESSAGES.USERNAME_TAKEN);
+	}
+
+	user = await User.findOne({ email: email });
+
+	if (user) {
+		errors.push(ERROR_MESSAGES.EMAIL_TAKEN);
+	}
+
+    if (errors.length > 0) {
+        throw errors;
+    }
+
+	return User.create({ username, email, password, repeatPassword });
+};
 
 exports.login = async (username, password) => {
 	const user = await User.findOne({ username });
 
 	if (!user) {
-		throw new Error(
-			'Sorry, your username or password was incorrect. Please double-check your username and password.'
-		);
+		throw Error(ERROR_MESSAGES.INVALID_LOGIN);
 	}
 
 	const validPassword = await bcrypt.compare(password, user.password);
 
 	if (!validPassword) {
-		throw new Error(
-			'Sorry, your username or password was incorrect. Please double-check your username and password.'
-		);
+		throw Error(ERROR_MESSAGES.INVALID_LOGIN);
 	}
 
 	const payload = {
@@ -29,7 +67,9 @@ exports.login = async (username, password) => {
 		email: user.email,
 	};
 
-	const token = await jwtPromises.sign(payload, process.env.JWT_SECRET, { expiresIn: '2d' });
+	const token = await jwtPromises.sign(payload, process.env.JWT_SECRET, {
+		expiresIn: '2d',
+	});
 
 	return token;
 };
