3704fd765e2506d4209e5fb7a9507e458b7d4f9b
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-02-04 17:54:19 +0200

    Change the user data validation to always return the trimmed data even before the checking for existing users start

--- src/utils/commonValidation.js
+++ src/utils/commonValidation.js
@@ -9,36 +9,41 @@ function isRequired(value) {
 	return true;
 };
 
-async function userDataValidation(userData) {
+async function userDataValidation({ username, email, password, repeatPassword }) {
 	const result = {
         errors: [],
-        newUserData: undefined,
+        newUserData: { username, email, password, repeatPassword },
     };
 
     // Checking that there is data and most importantly it is not only whitespaces
 
-	if (isRequired(userData.username) === false) {
+	if (isRequired(username) === false) {
 		result.errors.push(ERROR_MESSAGES.REQUIRED('Username'));
-	}
+	} else {
+        result.newUserData.username = result.newUserData.username.trim();
+    }
 
-	if (isRequired(userData.email) === false) {
+	if (isRequired(email) === false) {
 		result.errors.push(ERROR_MESSAGES.REQUIRED('Email'));
-	}
+	} else {
+        result.newUserData.email = result.newUserData.email.trim();
+    }
 
-	if (isRequired(userData.password) === false) {
+	if (isRequired(password) === false) {
 		result.errors.push(ERROR_MESSAGES.REQUIRED('Password'));
-	}
+	} else {
+        result.newUserData.password = result.newUserData.password.trim();
+    }
+
+    if (isRequired(repeatPassword) === false) {
+		result.errors.push(ERROR_MESSAGES.REQUIRED('Repeat Password'));
+	} else {
+        result.newUserData.repeatPassword = result.newUserData.repeatPassword.trim();
+    }
 
 	if (result.errors.length > 0) {
 		return result;
 	}
-
-	result.newUserData = {
-		username: userData.username.trim(),
-		email: userData.email.trim(),
-		password: userData.password.trim(),
-		repeatPassword: userData.repeatPassword.trim(),
-	};
 
     // Checking that there is no user with the same username or email in db
 
