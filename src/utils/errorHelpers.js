04c8cd40183a52e2648085836b2b6e3ec1636d94
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-02-04 11:33:28 +0200

    Fix bug when extracting mongoose errors

--- src/utils/errorHelpers.js
+++ src/utils/errorHelpers.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 
 exports.extractErrorMessages = (err) => {
-    if (err instanceof mongoose.MongooseError) {
-        return Object.values(err.errors).map(x => x.message);
-    } else if (err instanceof Error) {
-        return [err.message];
-    }
+	if (err instanceof mongoose.MongooseError) {
+		if (err.errors) {
+			return Object.values(err.errors).map((x) => x.message);
+		} else {
+            return [err.message];
+		}
+	} else {
+		return [err.message];
+	}
 };
