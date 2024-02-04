79bc974498b7768e6dfc1067de0054d131e3ff41
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-02-04 13:07:56 +0200

    Add the error messages from the constant file in Note model

--- src/models/Note.js
+++ src/models/Note.js
@@ -1,16 +1,18 @@
 const mongoose = require('mongoose');
 
+const { ERROR_MESSAGES } = require('../utils/errorMessages');
+
 const noteSchema = new mongoose.Schema({
 	title: {
 		type: String,
-		required: [true, 'Title is required'],
-		minLength: [4, 'Title should be at least 4 characters long'],
-		maxLength: [30, "Title can't be more than 30 characters long"],
+		required: [true, ERROR_MESSAGES.REQUIRED('Title')],
+		minLength: [4, ERROR_MESSAGES.MIN_LENGTH('Title', 4)],
+		maxLength: [30, ERROR_MESSAGES.MAX_LENGTH('Title', 30)],
 	},
 	description: {
 		type: String,
-		required: [true, 'Description is required'],
-		minLength: [5, 'Description should be at least 5 characters long'],
+		required: [true, ERROR_MESSAGES.REQUIRED('Description')],
+		minLength: [5, ERROR_MESSAGES.MIN_LENGTH('Description', 5)],
 	},
 	author: {
 		type: mongoose.Types.ObjectId,
