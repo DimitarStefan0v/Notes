4b3c687bae66ca62714160a60ddd8d7bb6c81e50
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-02-08 19:59:00 +0200

    Add js to show and hide backdrop

--- src/public/js/showNavigation.js
+++ src/public/js/showNavigation.js
@@ -1,14 +1,21 @@
 const menuBtn = document.getElementById('hamburger-menu');
 const links = document.getElementById('nav-links-mobile');
+const backdrop = document.getElementById('backdrop');
 
 menuBtn.addEventListener('click', () => {
-    console.log(menuBtn);
-    if (menuBtn.classList.contains('toggle')) {
-        menuBtn.classList.remove('toggle');
-        links.style.display = 'none';
-    } else {
-        menuBtn.classList.add('toggle');
-        links.style.display = 'flex';
-    }
-    
+	if (menuBtn.classList.contains('toggle')) {
+		menuBtn.classList.remove('toggle');
+		links.style.display = 'none';
+		backdrop.style.display = 'none';
+	} else {
+		menuBtn.classList.add('toggle');
+		links.style.display = 'flex';
+		backdrop.style.display = 'block';
+	}
+});
+
+backdrop.addEventListener('click', () => {
+	menuBtn.classList.remove('toggle');
+	links.style.display = 'none';
+	backdrop.style.display = 'none';
 });
