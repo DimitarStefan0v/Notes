8986b63070415a60d45471a4358fa3ac266a8511
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-02-10 18:31:08 +0200

    Add contacts endpoint in home controller on get method

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -6,4 +6,8 @@ router.get('/', (req, res) => {
 	res.render('home/home', { pageTitle: 'Home', path: '/' });
 });
 
+router.get('/contacts', (req, res) => {
+    res.render('home/contacts', { pageTitle: 'Contacts', path: '/contacts' });
+});
+
 module.exports = router;
