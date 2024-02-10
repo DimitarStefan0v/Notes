f64fad71f46d47b56d0d2e78928d6de8c11df4bb
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-02-10 19:02:34 +0200

    Add validation for email that it is not null or whitespace and return error if it is

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
+const { isRequired } = require('../utils/commonValidation');
+const { ERROR_MESSAGES } = require('../utils/errorMessages');
+const { extractErrorMessages } = require('../utils/errorHelpers');
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -11,12 +15,41 @@ router.get('/contacts', (req, res) => {
 		pageTitle: 'Contacts',
 		path: '/contacts',
 		errorMessages: [],
+		emailContent: {},
 	});
 });
 
 router.post('/contacts', (req, res) => {
-	const { email, message } = req.body;
-    console.log(email, message);
+	const emailContent = {
+		email: req.body.email,
+		message: req.body.message,
+	};
+
+	const errors = [];
+
+	isRequired(emailContent.email)
+		? (emailContent.email = emailContent.email.trim())
+		: errors.push(ERROR_MESSAGES.REQUIRED('Email'));
+	isRequired(emailContent.message)
+		? (emailContent.message = emailContent.message.trim())
+		: errors.push(ERROR_MESSAGES.REQUIRED('Message content'));
+
+	try {
+		if (errors.length > 0) {
+			throw errors;
+		}
+
+        
+		// TODO send email if there are no errors
+	} catch (error) {
+		const messages = extractErrorMessages(error);
+		return res.render('home/contacts', {
+			pageTitle: 'Contacts',
+			path: '/contacts',
+			errorMessages: messages,
+			emailContent,
+		});
+	}
 
     res.redirect('/');
 });
