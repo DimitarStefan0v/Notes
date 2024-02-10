71f24d803989a88ddd57b1175de4ad599f8fd740
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-02-10 20:12:53 +0200

    Commenting contacts endpoint cause i still don't have authenticated domain and can only send emails as single sender

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -3,6 +3,7 @@ const express = require('express');
 const { isRequired } = require('../utils/commonValidation');
 const { ERROR_MESSAGES } = require('../utils/errorMessages');
 const { extractErrorMessages } = require('../utils/errorHelpers');
+const emailsService = require('../services/emailsService');
 
 const router = express.Router();
 
@@ -10,58 +11,61 @@ router.get('/', (req, res) => {
 	res.render('home/home', { pageTitle: 'Home', path: '/' });
 });
 
-router.get('/contacts', (req, res) => {
-	res.render('home/contacts', {
-		pageTitle: 'Contacts',
-		path: '/contacts',
-		errorMessages: [],
-		emailContent: {},
-	});
-});
+// router.get('/contacts', (req, res) => {
+// 	res.render('home/contacts', {
+// 		pageTitle: 'Contacts',
+// 		path: '/contacts',
+// 		errorMessages: [],
+// 		emailContent: {},
+// 	});
+// });
 
-router.post('/contacts', (req, res) => {
-	const emailContent = {
-		email: req.body.email,
-		message: req.body.message,
-	};
+// router.post('/contacts', async (req, res) => {
+// 	const emailContent = {
+// 		email: req.body.email,
+// 		message: req.body.message,
+// 	};
 
-	const errors = [];
+// 	const errors = [];
 
-	isRequired(emailContent.email)
-		? (emailContent.email = emailContent.email.trim())
-		: errors.push(ERROR_MESSAGES.REQUIRED('Email'));
-	isRequired(emailContent.message)
-		? (emailContent.message = emailContent.message.trim())
-		: errors.push(ERROR_MESSAGES.REQUIRED('Message content'));
+// 	isRequired(emailContent.email)
+// 		? (emailContent.email = emailContent.email.trim())
+// 		: errors.push(ERROR_MESSAGES.REQUIRED('Email'));
+// 	isRequired(emailContent.message)
+// 		? (emailContent.message = emailContent.message.trim())
+// 		: errors.push(ERROR_MESSAGES.REQUIRED('Message content'));
 
-	try {
-		if (errors.length > 0) {
-			throw errors;
-		}
+// 	try {
+// 		if (errors.length > 0) {
+// 			throw errors;
+// 		}
 
-        if (emailContent.message.length < 10) {
-            errors.push(ERROR_MESSAGES.MIN_LENGTH('Message content', 10));
-            throw errors;
-        }
+// 		if (emailContent.message.length < 10) {
+// 			errors.push(ERROR_MESSAGES.MIN_LENGTH('Message content', 10));
+// 			throw errors;
+// 		}
 
-        const isEmailValid = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(emailContent.email);
-        if (!isEmailValid) {
-            errors.push(ERROR_MESSAGES.EMAIL_REGEX);
-            throw errors;
-        }
-        
-		// TODO send email if there are no errors
-	} catch (error) {
-		const messages = extractErrorMessages(error);
-		return res.render('home/contacts', {
-			pageTitle: 'Contacts',
-			path: '/contacts',
-			errorMessages: messages,
-			emailContent,
-		});
-	}
+// 		const isEmailValid =
+// 			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+// 				emailContent.email
+// 			);
+// 		if (!isEmailValid) {
+// 			errors.push(ERROR_MESSAGES.EMAIL_REGEX);
+// 			throw errors;
+// 		}
 
-    res.redirect('/');
-});
+//         await emailsService.send(process.env.MY_EMAIL, emailContent.email, emailContent.message);
+// 	} catch (error) {
+// 		const messages = extractErrorMessages(error);
+// 		return res.render('home/contacts', {
+// 			pageTitle: 'Contacts',
+// 			path: '/contacts',
+// 			errorMessages: messages,
+// 			emailContent,
+// 		});
+// 	}
+
+// 	res.redirect('/');
+// });
 
 module.exports = router;
